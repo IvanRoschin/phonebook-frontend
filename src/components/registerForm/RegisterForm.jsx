18a6cc7af5fc295ec9cd56154c0174786e23dba8
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-02-25 21:32:26 +0200

    adds registration in mongo

--- src/components/registerForm/RegisterForm.jsx
+++ src/components/registerForm/RegisterForm.jsx
@@ -1,10 +1,17 @@
 import { AuthForm } from 'components/authForm';
-// import { useRegisterUserMutation } from 'redux/auth/authSlice';
+import { useRegisterUserMutation } from 'redux/auth/authSlice';
 
 export const RegisterForm = () => {
-  //   const [registerUser] = useRegisterUserMutation();
-  const handleRegister = () => {
-    console.log('Hello');
+  const [registerUser] = useRegisterUserMutation();
+
+  const handleRegister = async values => {
+    try {
+      await registerUser(values);
+      //   toast.success(`${values.name} is added to Phonebook.`);
+    } catch (error) {
+      console.log(error.message);
+      //   toast.error('error');
+    }
   };
 
   return <AuthForm onSubmit={handleRegister} btnText={'Signup'} />;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from './auth/authSlice';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    authApi.middleware,
+  ],
 });
